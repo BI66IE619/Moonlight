07a6768ef294dc897956a1bd444481d7c113ff1e
biggie <146889677+BI66IE619@users.noreply.github.com>
2026-01-30 13:44:34 -0800

    Update uv.config.js

--- proxy/uv/uv.config.js
+++ proxy/uv/uv.config.js
@@ -1,6 +1,11 @@
+// Function to get the currently selected server from the UI or default
+const getBare = () => {
+    return localStorage.getItem('moonlight_bare') || 'https://bare.benroberts.dev/';
+};
+
 self.__uv$config = {
     prefix: '/proxy/uv/service/',
-    bare: 'https://bare.benroberts.dev/', 
+    bare: getBare(),
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/proxy/uv/uv.handler.js',
