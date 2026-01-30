c8d681b08dbfdc14bac00d5d2dd11e1e371696b6
biggie <146889677+BI66IE619@users.noreply.github.com>
2026-01-30 13:40:50 -0800

    Add files via upload

--- proxy/uv/uv.config.js
+++ proxy/uv/uv.config.js
@@ -1,10 +1,11 @@
+/*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/proxy/uv/service/',
-    bare: 'https://bare.benroberts.dev/', // This is a 2026 active bare server
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/proxy/uv/uv.handler.js',
-    bundle: '/proxy/uv/uv.bundle.js',
-    config: '/proxy/uv/uv.config.js',
-    sw: '/proxy/uv/uv.sw.js',
+	prefix: "/service/",
+	encodeUrl: Ultraviolet.codec.xor.encode,
+	decodeUrl: Ultraviolet.codec.xor.decode,
+	handler: "/uv.handler.js",
+	client: "/uv.client.js",
+	bundle: "/uv.bundle.js",
+	config: "/uv.config.js",
+	sw: "/uv.sw.js",
 };
